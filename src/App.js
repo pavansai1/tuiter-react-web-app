2874df646009284fe53e7aedcf41c4bd3704aea3
PAVANSAI KUMAR ALLADI <alladi.p@northeastern.edu>
2023-01-22 18:11:06 -0500

    Updated content in App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 123
+  const b = 234
+  let c = a + b
+  const d = [a, b, c]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Edit <code>src/App.js</code> and save to reload.
-        </h1>
+        <p>
+          Welcome to Web Dev CS5610 Section 6.
+        </p>
+        <p>
+          a = {a}
+          <br/>
+          b = {b}
+        <br/>
+          c = {c}
+          <br/>
+          d = {d}
+          <ul>
+            <li>{d[0]}</li>
+            <li>{d[1]}</li>
+            <li>{d[2]}</li>
+          </ul>
+          <ol>
+            {d.map(item => <li>{item}</li>)}
+          </ol>
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
